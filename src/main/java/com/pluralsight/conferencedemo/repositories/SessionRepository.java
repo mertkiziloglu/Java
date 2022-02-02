9ecf435ae64805530b7882d49dcb4b84d1a7f7ed
Mert kiziloglu <mert_besiktas24@hotmail.com>
2022-02-02 09:39:00 +0300

    *Create classes project-start

--- src/main/java/com/pluralsight/conferencedemo/repositories/SessionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight.conferencedemo.repositories;
-
-import com.pluralsight.conferencedemo.model.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<Session ,Long> {
-}
--- src/main/java/com/pluralsight/conferencedemo/repositories/SpeakersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight.conferencedemo.repositories;
-
-import com.pluralsight.conferencedemo.model.Speaker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpeakersRepository extends JpaRepository<Speaker,Long> {
-}
