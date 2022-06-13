3533411d55f91f75a16633c4dffd9ade29a070f9
Mert kiziloglu <mert_besiktas24@hotmail.com>
2022-06-13 12:16:34 +0300

    MaxMemoryIndicator

---
+++ todo-app-master/src/main/java/io/kiziloglu/mert/todoapp/health/MaxMemoryIndicator.java
@@ -0,0 +1,16 @@
+package io.kiziloglu.mert.todoapp.health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MaxMemoryIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        boolean invalid = Runtime.getRuntime().maxMemory() < (100 * 1024 * 1024);
+        Status status = invalid ? Status.DOWN : Status.UP;
+        return new Health.Builder(status).build();
+    }
+}
