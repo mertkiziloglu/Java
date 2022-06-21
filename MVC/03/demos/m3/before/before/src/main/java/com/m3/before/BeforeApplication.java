e3c7435d93be8c38f68bd7d653eb2b8fc489154a
Mert kiziloglu <mert_besiktas24@hotmail.com>
2022-06-21 17:07:46 +0300

    before example

---
+++ MVC/03/demos/m3/before/before/src/main/java/com/m3/before/BeforeApplication.java
@@ -0,0 +1,13 @@
+package com.m3.before;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeforeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeforeApplication.class, args);
+    }
+
+}
---
+++ MVC/03/demos/m3/before/before/src/test/java/com/m3/before/BeforeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m3.before;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeforeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
