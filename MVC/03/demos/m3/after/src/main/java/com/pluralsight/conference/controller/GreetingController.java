9580988a3284657ccd67eae64e78368a1d50ffb2
Mert kiziloglu <mert_besiktas24@hotmail.com>
2022-06-24 09:17:25 +0300

    example 4

--- MVC/03/demos/m3/after/src/main/java/com/pluralsight/conference/controller/GreetingController.java
+++ MVC/03/demos/m3/after/src/main/java/com/pluralsight/conference/controller/GreetingController.java
@@ -10,7 +10,8 @@ public class GreetingController {
 
     @GetMapping("greeting")
     public String greeting (Map<String, Object> model) {
-        model.put("message", "Hello Bryan");
+        model.put("message", "Hello ");
+        model.put("name", "Meroot");
         return "greeting";
     }
 }
--- MVC/03/demos/m3/after/src/main/webapp/WEB-INF/jsp/greeting.jsp
+++ MVC/03/demos/m3/after/src/main/webapp/WEB-INF/jsp/greeting.jsp
@@ -6,5 +6,6 @@
 </head>
 <body>
 <h1>${message}</h1>
+<h2>${name}</h2>
 </body>
 </html>
