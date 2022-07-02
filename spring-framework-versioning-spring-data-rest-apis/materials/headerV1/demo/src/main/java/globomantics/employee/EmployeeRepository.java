e2cbe007c7a55db8100ae2edc3e089b9d5f389e0
Mert kiziloglu <mert_besiktas24@hotmail.com>
2022-07-02 22:35:41 +0300

    data rest api

---
+++ spring-framework-versioning-spring-data-rest-apis/materials/headerV1/demo/src/main/java/globomantics/employee/EmployeeRepository.java
@@ -0,0 +1,4 @@
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
+}
---
+++ spring-framework-versioning-spring-data-rest-apis/materials/headerV1/demo/src/main/java/globomantics/employee/ExampleService.java
@@ -0,0 +1,14 @@
+import org.springframework.data.rest.webmvc.BasePathAwareController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@BasePathAwareController
+public class ExampleService {
+
+    @RequestMapping("/schedule")
+    public String getSchedule() {
+
+        return "Your v1 schedule is M-F 9-9";
+    }
+}
